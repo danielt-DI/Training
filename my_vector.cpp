6110db56baa20283af3b0163a44b16f557ce6433
Daniel Tomchin <daniel@training.com>
2022-03-22 14:41:39 +0200

    Initial commit

---
+++ my_vector.cpp
@@ -0,0 +1,9 @@
+//
+// Created by daniel on 22/03/2022.
+//
+
+#include "my_vector.h"
+
+my_vector::my_vector() {}
+
+my_vector::~my_vector() {}
---
+++ my_vector.h
@@ -0,0 +1,17 @@
+//
+// Created by daniel on 22/03/2022.
+//
+
+#ifndef CPP_TRAINING_MY_VECTOR_H
+#define CPP_TRAINING_MY_VECTOR_H
+
+
+class my_vector {
+public:
+    my_vector();
+
+    virtual ~my_vector();
+};
+
+
+#endif //CPP_TRAINING_MY_VECTOR_H
