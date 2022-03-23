31daa317fa439830480c636146ec65012889fb86
Daniel Tomchin <daniel@training.com>
2022-03-23 16:36:52 +0200

    implemented my_vector (int only)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include "my_vector.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    my_vector v(3);
+    v.push_back(5);
+    v.push_back(5);
+    v.push_back(5);
+    v.push_back(5);
+    v.print();
+
+    std::cout << std::endl;
+
+    my_vector vv(v);
+    vv.push_back(10);
+    vv.print();
+    v.print();
+
+    std::cout << "Finished" << std::endl;
+
     return 0;
 }
--- my_vector.h
+++ my_vector.h
@@ -2,16 +2,91 @@
 // Created by daniel on 22/03/2022.
 //
 
+#include <iostream>
+
 #ifndef CPP_TRAINING_MY_VECTOR_H
 #define CPP_TRAINING_MY_VECTOR_H
 
-
 class my_vector {
-public:
-    my_vector();
+    int* buff;
+    int vec_capacity;
+    int vec_size;
 
+public:
+    // constructor
+    my_vector(int capacity): vec_capacity(capacity), vec_size(0), buff(new int[capacity]) {}
+
+    // copy constructor
+    my_vector(const my_vector& vec);
+
+    // copy assigment operator
+    my_vector& operator=(const my_vector& vec);
+
+    // move constructor
+    my_vector(my_vector&& vec);
+
+    // destructor
     virtual ~my_vector();
+
+    // vector interface
+    int size() {return vec_size;}
+    void push_back(int elem);
+    void print();
 };
 
+// copy constructor
+my_vector::my_vector(const my_vector &vec): vec_capacity(vec.vec_capacity), vec_size(vec.vec_size), buff(new int[vec.vec_capacity]) {
+    for(int i = 0; i < vec_size; i++) {
+        buff[i] = vec.buff[i];
+    }
+}
+
+// assignment operator
+my_vector& my_vector::operator=(const my_vector &vec) {
+    vec_size = vec.vec_size;
+    vec_capacity = vec.vec_capacity;
+    delete[] buff;
+    buff = new int[vec.vec_capacity];
+
+    for(int i = 0; i < vec_size; i++) {
+        buff[i] = vec.buff[i];
+    }
+}
+
+// move constructor
+my_vector::my_vector(my_vector &&vec):vec_size(vec.vec_size), vec_capacity(vec.vec_capacity), buff(new int[vec.vec_capacity]) {
+    vec.buff = nullptr;
+}
+
+// destructor
+my_vector::~my_vector() {
+    delete[] buff;
+}
+
+// vector interface
+void my_vector::push_back(int elem) {
+    if(vec_size == vec_capacity) {
+        vec_capacity *= 2;
+        int* tmp = new int[vec_capacity];
+
+        for(int i = 0; i < vec_size; i++) {
+            tmp[i] = buff[i];
+        }
+
+        delete[] buff;
+
+        buff = tmp;
+    }
+
+    buff[vec_size++] = elem;
+}
+
+void my_vector::print() {
+    for(int i = 0; i < vec_size; i++) {
+        std::cout << buff[i] << " ";
+    }
+
+    std::cout << std::endl;
+}
 
 #endif //CPP_TRAINING_MY_VECTOR_H
